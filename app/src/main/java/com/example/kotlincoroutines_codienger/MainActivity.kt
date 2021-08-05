621b87ffc2904890dac7b5699c531953215d2392
Sanjay45 <sanjay>
2021-08-05 22:57:52 +0530

    tested heavy task on ui thread

--- app/src/main/java/com/example/kotlincoroutines_codienger/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutines_codienger/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.kotlincoroutines_codienger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.kotlincoroutines_codienger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn.setOnClickListener {
+            doSomeTask()
+        }
+    }
+
+    private fun doSomeTask() {
+        Thread.sleep(5000)
+        Toast.makeText(this,"Task Done",Toast.LENGTH_LONG).show()
     }
 }
