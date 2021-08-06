de3b84994aa759870985a297eb24291b43b0aa53
Sanjay45 <sanjay>
2021-08-07 00:06:25 +0530

    used withConext() to switch thread

--- app/src/main/java/com/example/kotlincoroutines_codienger/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutines_codienger/MainActivity.kt
@@ -20,32 +20,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        lifecycleScope.launch {
-            //testLaunch()
-            testAsync()
+        lifecycleScope.launch(Dispatchers.IO) {
+           val result = networkCall()
+            withContext(Dispatchers.Main) {
+                binding.textView.text = result
+            }
         }
 
     }
 
-    private suspend fun testLaunch() {
-       val job =  lifecycleScope.launch {
-            delay(5000)
-            Log.i("MainActivity","5 seconds to finish")
-        }
-        // If want to run this "test: " after delaying 5 sec then we have to use  job.join()
-        job.join()
-        Log.i("MainActivity","testLaunch: ")
+    private suspend fun networkCall(): String {
+        Log.i("MainActivity","networkCall: start")
+        delay(5000)
+        Log.i("MainActivity","networkCall: end")
+        return "Result"
     }
-
-    private suspend fun testAsync() {
-        val deferred: Deferred<String> = lifecycleScope.async {
-            delay(5000)
-            Log.i("MainActivity","5 seconds to finish")
-            "Return value of async()"
-        }
-
-      val value = deferred.await()
-        Log.i("MainActivity","testAsync: $value")
-    }
-
 }
